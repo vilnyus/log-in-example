cf666286aed2576ebaf3476070854ac9453444b7
vilnyus <govagharsh@gmail.com>
2019-12-19 16:28:47 +0400

    signup post

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
-var express=require("express"); 
-var bodyParser=require("body-parser"); 
+var express = require('express');
+var bodyParser = require('body-parser');
 var cons = require('consolidate');
-var path = require('path');
-var bcrypt = require('bcrypt');
-
+// var path = require('path');
+var bcrypt = require('bcrypt-nodejs');
 
 const PORT = 3000;
+const saltRounds = 10;
 
-const mongoose = require('mongoose'); 
-mongoose.connect('mongodb://user-abc:user123@ds353748.mlab.com:53748/users_db_auth', { useNewUrlParser: true, useUnifiedTopology: true }); 
-var db=mongoose.connection; 
-db.on('error', console.log.bind(console, "connection error")); 
-db.once('open', function(callback){ 
-	console.log("connection succeeded"); 
-}) 
+const mongoose = require('mongoose');
+mongoose.connect(
+  'mongodb://user-abc:user123@ds353748.mlab.com:53748/users_db_auth',
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
 
-var app=express(); 
+var db = mongoose.connection;
+db.on('error', console.log.bind(console, 'connection error'));
+db.once('open', function(callback) {
+  console.log('connection succeeded');
+});
 
+var app = express();
+// app.use(express.static('public'));
 app.use(express.static(__dirname + '/'));
 
 // app.use('*/views',express.static(path.join(__dirname, 'public/views')));
@@ -27,50 +31,54 @@ app.use(express.static(__dirname + '/'));
 app.engine('html', cons.swig);
 app.set('view engine', 'html');
 
-app.use(bodyParser.urlencoded({ 
-	extended: true
-})); 
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 
-app.post('sign_up', function(req,res){ 
-	var name = req.body.name; 
-	var email =req.body.email; 
-	var pass = req.body.password; 
-	var phone =req.body.phone; 
+app.post('/sign_up', function(req, res) {
+  var name = req.body.name;
+  var email = req.body.email;
+  var pass = req.body.password;
+  var phone = req.body.phone;
+  var salt = bcrypt.genSaltSync();
+  var password_hash = bcrypt.hashSync(pass, salt);
 
-	bcrypt.hash(pass, saltRounds, function(err, hash) {
-		console.log(pass);
-	  });
+  var user = {
+    _id: name,
+    email: email,
+    password: password_hash,
+    phone: phone
+  };
 
-	var data = { 
-		"name": name, 
-		"email":email, 
-		"password":pass, 
-		"phone":phone 
-	} 
-    
-    db.collection('users_db').insertOne(data, function(err, collection){ 
-		if (err) throw err; 
-		console.log("Record inserted Successfully"); 			
-	}); 
-		
-	return res.redirect('/signup_success.html'); 
-}) 
+  db.collection('users_db').insertOne(user, function(err, collection) {
+    if (err) throw err;
+    console.log('Record inserted Successfully');
+  });
+
+  return res.redirect('/signup_success.html');
+});
+
+app.post('/log_in', function(req, res) {
+  var name = req.body.name;
+  var pass = req.body.password;
+});
 
 app.get('/login', function(req, res) {
-	console.log("display login page");
-	res.redirect('/login.html');
-})
-
-// app.get('/signup', function(req, res) {
-//     res.render('./signup');
-// })
-
-app.get('/',function(req,res){ 
-	console.log("display index page");
-	res.sendFile('/index.html'); 
+  console.log('display login page');
+  res.redirect('/login.html');
 });
 
-app.listen(PORT, ()=> {
-    console.log(`Server is running on port ${PORT}` );
+app.get('/signup', function(req, res) {
+  res.render('./signup');
 });
 
+app.get('/', function(req, res) {
+  console.log('display index page');
+  res.sendFile('/index.html');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
